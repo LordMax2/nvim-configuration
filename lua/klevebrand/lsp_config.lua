9365e0daec954a6de8e6384ef1f9446cb2364aed
LordMax2 <max.klevebrand@gmail.com>
2025-07-03 23:36:12 +0200

    Stuff

--- lua/klevebrand/lsp_config.lua
+++ lua/klevebrand/lsp_config.lua
@@ -12,25 +12,17 @@ local on_attach = function(_, bufnr)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
 end
 
+-- html lsp
+lspconfig.html.setup({
+  capabilities = require('cmp_nvim_lsp').default_capabilities(), -- optional for nvim-cmp
+  filetypes = { "html", "htmldjango" }, -- add more as needed
+})
+
 -- rust-analyzer
 lspconfig.rust_analyzer.setup {
   capabilities = cmp_nvim_lsp.default_capabilities(),
   on_attach = on_attach,
 }
-vim.lsp.config("roslyn", {
-    on_attach = function()
-        print("This will run when the server attaches!")
-    end,
-    settings = {
-        ["csharp|inlay_hints"] = {
-            csharp_enable_inlay_hints_for_implicit_object_creation = true,
-            csharp_enable_inlay_hints_for_implicit_variable_types = true,
-        },
-        ["csharp|code_lens"] = {
-            dotnet_enable_references_code_lens = true,
-        },
-    },
-})
 
 -- Razzmatazz csharp-ls for C#
 lspconfig.csharp_ls.setup {
@@ -39,18 +31,6 @@ lspconfig.csharp_ls.setup {
   capabilities = cmp_nvim_lsp.default_capabilities(),
   on_attach = on_attach,
   root_dir = lspconfig.util.root_pattern("*.sln", "*.csproj", ".git"),
-}
-
-lspconfig.rzls.setup {
-  cmd = { "rzls" }, -- or full path to binary if not in PATH
-  filetypes = { "razor", "cshtml" },
-  root_dir = lspconfig.util.root_pattern("*.sln", "*.csproj", ".git"),
-  capabilities = require("cmp_nvim_lsp").default_capabilities(),
-  on_attach = function(client, bufnr)
-    local opts = { noremap=true, silent=true, buffer=bufnr }
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-  end,
 }
 
 cmp.setup({
@@ -97,3 +77,4 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 -- Setup nvim-cmp and luasnip
 local cmp = require('cmp')
 local luasnip = require('luasnip')
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
